e053396c494a5159383ddee0cc285f9fb606a599
vanajmoorthy <vanajmoorthy@gmail.com>
2020-08-23 08:03:59 +0530

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,32 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+
+    extends: ["eslint:recommended", "plugin:vue/essential"],
+
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module"
+    },
+
+    plugins: ["vue"],
+
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    },
+
+    root: true
+};
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Just a completely unstyled Todo List</h1>
+        <form @submit.prevent="addNewTodo()">
+            <input v-model="todo" placeholder="Add new todo" />
+            <input type="submit" />
+        </form>
+        <div class="todo" v-for="(todo, i) in todos" v-bind:key="todo">
+            <p :class="{ isDone: todo.done }">{{ todo.title }}</p>
+            <div>
+                <button @click="deleteTodo(i)">Delete</button>
+                <button @click="markDone(todo)" v-if="!todo.done">Done</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            todo: "",
+            todos: []
+        };
+    },
+    methods: {
+        addNewTodo() {
+            this.todos.push({ title: this.todo, done: false });
+            this.todo = "";
+        },
+        markDone(todo) {
+            todo.done = true;
+        },
+        deleteTodo(index) {
+            this.todos.splice(index, 1);
+        }
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    max-width: 20rem;
+    margin: 4rem auto;
+}
+
+.todo {
+    display: flex;
+    justify-content: space-between;
+    background-color: #999999;
+    margin: 2rem 0;
+    padding: 1rem 1rem;
+}
+
+.todo div {
+    display: flex;
+}
+
+.isDone {
+    text-decoration: line-through;
 }
 </style>
